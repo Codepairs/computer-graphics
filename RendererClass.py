8f68f1f2415ab0691c265dedad17d967caaaf15b
Sergey <goosewww010@gmail.com>
2024-04-01 16:12:58 +0400

    delete methods from labs 1,2

--- RendererClass.py
+++ RendererClass.py
@@ -12,161 +12,6 @@ class Renderer:
         self._image_matrix = np.zeros(self._image_size, dtype=np.uint8)
 
     @staticmethod
-    def update_point(image: np.ndarray, pos_y, pos_x, color) -> None:
-        """
-        Обновляет цвет точки по позиции
-        :param image:
-        :param pos_y: Позиция Y
-        :param pos_x: Позиция X
-        :param color: Цвет
-        :return: None
-        """
-        image[pos_y][pos_x] = color
-
-    @staticmethod
-    def make_image_colored(image: np.ndarray, color) -> None:
-        """
-        Заполняет изображение цветом color
-        :return: PIL.image
-        """
-        image.fill(color)
-
-    @staticmethod
-    def make_image_gradient(image: np.ndarray) -> None:
-        """
-
-        """
-        for y in np.arange(0, image.shape[0]):
-            for x in np.arange(0, image.shape[1]):
-                image[y, x] = ((y + x) // 2) % 256
-
-    @staticmethod
-    def algorithm_dotted_line(image: np.ndarray, x0, y0, x1, y1, count, color) -> None:
-        """
-        :param color:
-        :param count:
-        :param y1:
-        :param x1:
-        :param x0:
-        :param y0:
-        :param image: numpy ndarray
-        x = x1 + (1-t)(x0-x1)
-        недостатки:
-            из-за фиксированного count можем
-                            либо нарисовать один пиксель несколько раз
-                            либо не нарисовать вообще...
-        """
-        step = 1.0 / count
-        for t in np.arange(0, 1, step):
-            x = round((1.0 - t) * x0 + t * x1)
-            y = round((1.0 - t) * y0 + t * y1)
-            image[y, x] = color
-
-    @staticmethod
-    def algorithm_dotted_line_sqrt(image: np.ndarray, x0, y0, x1, y1, color) -> None:
-        """
-        :param color:
-        :param y1:
-        :param x1:
-        :param y0:
-        :param x0:
-        :param image: numpy ndarray
-        недостатки:
-            медленное вычисление корня
-        """
-        count = math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
-        step = 1.0 / count
-        for t in np.arange(0, 1, step):
-            x = round((1.0 - t) * x0 + t * x1)
-            y = round((1.0 - t) * y0 + t * y1)
-            image[y, x] = color
-
-    @staticmethod
-    def algorithm_x_loop_line(image: np.ndarray, x0, y0, x1, y1, color) -> None:
-        """
-        :param color:
-        :param y1:
-        :param x1:
-        :param y0:
-        :param x0:
-        :param image: numpy ndarray
-        недостатки:
-            если начало правее конца, то не рисует (без левой половины)
-            идем по x -> шаг по x<чем по у -> рядом с Оу получим пунктир
-        """
-        for x in range(x0, x1):
-            t = (x - x0) / (x1 - x0)
-            y = int(y1 * t + y0 * (1.0 - t))
-            image[y, x] = color
-
-    @staticmethod
-    def algorithm_x_loop_line_fixed(image: np.ndarray, x0, y0, x1, y1, color) -> None:
-        """
-        :param x0:
-        :param y0:
-        :param x1:
-        :param y1:
-        :param color:
-        :param image: numpy ndarray
-        недостатки:
-            отсутствуют в явном виде
-        """
-        xchange = False
-        if abs(x0 - x1) < abs(y0 - y1):
-            x0, y0 = y0, x0
-            x1, y1 = y1, x1
-            xchange = True
-
-        if x0 > x1:
-            x0, x1, = x1, x0
-            y0, y1 = y1, y0
-
-        for x in range(x0, x1):
-            t = (x - x0) / (x1 - x0)
-            y = int(y1 * t + y0 * (1.0 - t))
-            if xchange:
-                image[x, y] = color
-            else:
-                image[y, x] = color
-
-    @staticmethod
-    def algorithm_dy(image: np.ndarray, x0, y0, x1, y1, color) -> None:
-        """
-        :param x0:
-        :param y0:
-        :param x1:
-        :param y1:
-        :param color:
-        :param image: numpy ndarray
-        derror - погрешность вычислений, накапливаем ее, чтобы перейти вверх по y,
-         затем сбрасываем
-        """
-        xchange = False
-        if (abs(x0 - x1) < abs(y0 - y1)):
-            x0, y0 = y0, x0
-            x1, y1 = y1, x1
-            xchange = True
-
-        if (x0 > x1):
-            x0, x1 = x1, x0
-            y0, y1 = y1, y0
-
-        y = y0
-        dy = abs((y1 - y0) / (x1 - x0))
-        derror = 0.0
-        y_update = 1 if y1 > y0 else -1
-
-        for x in range(x0, x1):
-            derror += dy
-            if derror > 0.5:
-                derror -= 1.0
-                y += y_update
-            if xchange:
-                image[x, y] = color
-            else:
-                image[y, x] = color
-
-    @staticmethod
     def algorithm_bresenham(image: np.ndarray, x0, y0, x1, y1, color) -> None:
         """
         :param x0:
@@ -202,35 +47,6 @@ class Renderer:
                 image[y, x] = color
 
     @staticmethod
-    def draw_polygon_face(image: Image, model: ObjModelClass.ObjModel, color: list[int], polygon_number: int):
-        """
-        Отрисовка полигона
-        :param image:
-        :param model:
-        :param color:
-        :return:
-        """
-        point1, point2, point3 = model.get_points_by_index(polygon_number)
-        point1 = point1.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point2 = point2.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point3 = point3.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        Renderer.algorithm_bresenham(image, point1.x, point1.y, point2.x, point2.y, color)
-        Renderer.algorithm_bresenham(image, point2.x, point2.y, point3.x, point3.y, color)
-        Renderer.algorithm_bresenham(image, point3.x, point3.y, point1.x, point1.y, color)
-
-    @staticmethod
-    def draw_model_with_faces(image: Image, model: ObjModelClass.ObjModel, color: list[int]):
-        """
-        Отрисовка модели циклом по полигонам
-        :param image:
-        :param model:
-        :param color:
-        :return:
-        """
-        for i in range(1, len(model.faces) + 1):
-            Renderer.draw_polygon_face(image, model, color, i)
-
-    @staticmethod
     def determine_baricentric(x: int, x0: float, x1: float, x2: float, y: int, y0: float, y1: float,
                               y2: float) -> np.array:
         '''
@@ -250,74 +66,7 @@ class Renderer:
 
         return np.array([lambda0, lambda1, lambda2])
 
-    @staticmethod
-    def draw_triangle(image: np.ndarray, color: int, x0: float, x1: float, x2: float, y0: float, y1: float,
-                      y2: float) -> None:
-        xmin = min(x0, x1, x2)
-        ymin = min(y0, y1, y2)
-        xmax = max(x0, x1, x2)
-        ymax = max(y0, y1, y2)
 
-        if xmin < 0:
-            xmin = 0
-        if ymin < 0:
-            ymin = 0
-
-        if xmax > image.shape[1]:
-            xmax = image.shape[1]
-        if ymax > image.shape[0]:
-            ymax = image.shape[0]
-
-        for y in np.arange(ymin, ymax):
-            for x in np.arange(xmin, xmax):
-                baricentrics = Renderer.determine_baricentric(x, x0, x1, x2, y, y0, y1, y2)
-                # print(y, '  ', x, '  ', baricentrics)
-
-                if all(lam >= 0 for lam in baricentrics):
-                    image[y, x] = color
-
-    @staticmethod
-    def draw_model_with_random_color_polygons(image: Image, model: ObjModelClass.ObjModel):
-        """
-        Отрисовка модели циклом по полигонам
-        :param image:
-        :param model:
-        :param color:
-        :return:
-        """
-        for i in range(1, len(model.faces) + 1):
-            Renderer.draw_model_triangle(image, model, i)
-
-    @staticmethod
-    def draw_model_triangle(image: np.ndarray, model: ObjModelClass.ObjModel, polygon_number: int) \
-            -> None:
-        point1, point2, point3 = model.get_points_by_index(polygon_number)
-        point1 = point1.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point2 = point2.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point3 = point3.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-
-        x_min = min(point1.x, point2.x, point3.x)
-        y_min = min(point1.y, point2.y, point3.y)
-        x_max = max(point1.x, point2.x, point3.x)
-        y_max = max(point1.y, point2.y, point3.y)
-
-        if x_min < 0:
-            x_min = 0
-        if y_min < 0:
-            y_min = 0
-
-        if x_max > image.shape[1]:
-            x_max = image.shape[1]
-        if y_max > image.shape[0]:
-            y_max = image.shape[0]
-
-        random_color = colors.get_random_color()
-        for y in np.arange(y_min, y_max):
-            for x in np.arange(x_min, x_max):
-                baricentrics = Renderer.determine_baricentric(x, point1.x, point2.x, point3.x, y, point1.y, point2.y,
-                                                              point3.y)
-                if all(lam >= 0 for lam in baricentrics):
-                    image[y, x] = random_color
 
     @staticmethod
     def calculate_normal_to_triangle(point0, point1, point2) -> np.array:
@@ -335,7 +84,9 @@ class Renderer:
         return result
 
     @staticmethod
-    def calculate_cos_to_triangle(point1, point2, point3, light_direction_vector:np.array) -> float:
+    def calculate_cos_to_triangle(point1, point2, point3, light_direction_vector=None) -> float:
+        if light_direction_vector is None:
+            light_direction_vector = [0.0, 0.0, 0.1]
         normal_coordinates = Renderer.calculate_normal_to_triangle(point1, point2, point3)
         norma_normal_to_triangle = np.linalg.norm(normal_coordinates)
 
@@ -348,94 +99,6 @@ class Renderer:
 
         return result
 
-    @staticmethod
-    def draw_model_with_random_color_and_cos(image: Image, model: ObjModelClass.ObjModel):
-        """
-        Отрисовка модели циклом по полигонам
-        :param image:
-        :param model:
-        :return:
-        """
-        for i in range(1, len(model.faces) + 1):
-            Renderer.draw_model_triangle_with_cos(image, model, i)
-            # print(f"Итерация {i}")
-
-    @staticmethod
-    def draw_model_triangle_with_cos(image: np.ndarray, model: ObjModelClass.ObjModel, polygon_number: int) \
-            -> None:
-        point1, point2, point3 = model.get_points_by_index(polygon_number)
-        point1 = point1.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point2 = point2.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point3 = point3.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-
-        x_min = min(point1.x, point2.x, point3.x)
-        y_min = min(point1.y, point2.y, point3.y)
-        x_max = max(point1.x, point2.x, point3.x)
-        y_max = max(point1.y, point2.y, point3.y)
-
-        if x_min < 0:
-            x_min = 0
-        if y_min < 0:
-            y_min = 0
-
-        if x_max > image.shape[1]:
-            x_max = image.shape[1]
-        if y_max > image.shape[0]:
-            y_max = image.shape[0]
-
-        random_color = colors.get_random_color()
-        light_cos = Renderer.calculate_cos_to_triangle(point1, point2, point3)
-        for y in np.arange(y_min, y_max):
-            for x in np.arange(x_min, x_max):
-                baricentrics = Renderer.determine_baricentric(x, point1.x, point2.x, point3.x, y, point1.y, point2.y,
-                                                              point3.y)
-                if all(lam >= 0 and light_cos < 0 for lam in baricentrics):
-                    image[y, x] = random_color
-
-    @staticmethod
-    def draw_model_with_light(image: Image, color: list[int], model: ObjModelClass.ObjModel):
-        """
-        Отрисовка модели циклом по полигонам
-        :param image:
-        :param model:
-        :return:
-        """
-        for i in range(1, len(model.faces) + 1):
-            Renderer.draw_model_triangle_with_light(image, color, model, i)
-            # print(f"Итерация {i}")
-
-    @staticmethod
-    def draw_model_triangle_with_light(image: np.ndarray, color: list[int], model: ObjModelClass.ObjModel,
-                                       polygon_number: int) \
-            -> None:
-        point1, point2, point3 = model.get_points_by_index(polygon_number)
-        point1 = point1.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point2 = point2.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-        point3 = point3.transform_to_int(model.scale, model.offset_x, model.offset_y, model.offset_z)
-
-        x_min = min(point1.x, point2.x, point3.x)
-        y_min = min(point1.y, point2.y, point3.y)
-        x_max = max(point1.x, point2.x, point3.x)
-        y_max = max(point1.y, point2.y, point3.y)
-
-        if x_min < 0:
-            x_min = 0
-        if y_min < 0:
-            y_min = 0
-
-        if x_max > image.shape[1]:
-            x_max = image.shape[1]
-        if y_max > image.shape[0]:
-            y_max = image.shape[0]
-
-        light_cos = Renderer.calculate_cos_to_triangle(point1, point2, point3)
-        color_with_light = [-item * light_cos for item in color]
-        for y in np.arange(y_min - 1, y_max + 1):
-            for x in np.arange(x_min - 1, x_max + 1):
-                baricentrics = Renderer.determine_baricentric(x, point1.x, point2.x, point3.x, y, point1.y, point2.y,
-                                                              point3.y)
-                if all(lam >= 0 and light_cos < 0 for lam in baricentrics):
-                    image[y, x] = color_with_light
 
     @staticmethod
     def draw_triangle_zbuffer(image: np.ndarray, color: list[int], model: ObjModelClass.ObjModel, polygon_number: int,
@@ -635,21 +298,22 @@ class Renderer:
                                                 zbuffer: np.ndarray):
         point1, point2, point3 = model.get_points_by_index(polygon_number)
 
-        model.scale = 2000
-        point1.z += model.offset_z
-        point2.z += model.offset_z
-        point3.z += model.offset_z
-
         point1.x, point1.y = (point1.x / point1.z), (point1.y / point1.z)
         point2.x, point2.y = (point2.x / point2.z), (point2.y / point2.z)
         point3.x, point3.y = (point3.x / point3.z), (point3.y / point3.z)
 
         point1.x, point1.y = point1.x * model.scale + model.offset_x, \
-                             (point1.y * model.scale + model.offset_y // 2)
+            (point1.y * model.scale + model.offset_y)
         point2.x, point2.y = (point2.x * model.scale + model.offset_x), (
-                point2.y * model.scale + model.offset_y // 2)
+                point2.y * model.scale + model.offset_y)
         point3.x, point3.y = (point3.x * model.scale + model.offset_x), (
-                point3.y * model.scale + model.offset_y // 2)
+                point3.y * model.scale + model.offset_y)
+
+        point1.z += model.offset_z + 10000
+        point2.z += model.offset_z + 10000
+        point3.z += model.offset_z + 10000
+
+
         Renderer.draw_with_light(zbuffer, point1, point2, point3, image, color)
 
     @staticmethod
@@ -663,4 +327,4 @@ class Renderer:
         """
         for i in range(1, len(model.faces) + 1):
             Renderer.draw_triangle_projective_transformation(image, color, model, i, zbuffer)
-            # print(f"Итерация {i}")
+            #print(f"Итерация {i}")
--- TaskManagerClass.py
+++ TaskManagerClass.py
@@ -14,8 +14,9 @@ class TaskManager:
         path = 'obj-files/model_{}.obj'.format(model_num)
         model_to_render = ObjModel(obj_file=path)
         model_to_render.fill_coordinates_info()
-        model_to_render.scale_coordinates(resolution)
         model_to_render.offset_coordinates(resolution)
+        model_to_render.scale_coordinates(resolution)
+
         return model_to_render
 
     @staticmethod
@@ -26,193 +27,6 @@ class TaskManager:
         model_to_render.offset_coordinates(resolution)
         model_to_render.scale_coordinate_to_z(resolution)
         return model_to_render
-
-
-    @staticmethod
-    def task1(matrix_size: tuple):
-        matrix_size_3d = matrix_size + (3,)
-        renderer = Renderer()
-        black_image = np.ndarray(matrix_size, dtype=np.uint8)
-        white_image = np.ndarray(matrix_size, dtype=np.uint8)
-        volume_image = np.full(shape=matrix_size_3d, fill_value=[255, 0, 0], dtype=np.uint8)
-        gradient_image = np.ndarray(shape=matrix_size_3d, dtype=np.uint8)
-
-        renderer.make_image_colored(black_image, 0)
-
-        pil_black = Image.fromarray(black_image, mode="L")
-        pil_black.save('images/black-image.png')
-        pil_black.show()
-
-        renderer.make_image_colored(white_image, 255)
-
-        pil_white = Image.fromarray(white_image, mode="L")
-        pil_white.save('images/white-test.png')
-        pil_white.show()
-
-        pil_volume = Image.fromarray(volume_image, mode="RGB")
-        pil_volume.save('images/volume-test.png')
-        pil_volume.show()
-
-        renderer.make_image_gradient(gradient_image)
-
-        pil_gradient = Image.fromarray(gradient_image)
-        pil_gradient.save('images/gradient-test.png')
-        pil_gradient.show()
-
-    @staticmethod
-    def task2(matrix_size: tuple, color: int):
-        single_image = np.zeros(shape=matrix_size, dtype=np.uint8)
-        images = np.tile(single_image, (6, 1, 1))
-
-        renderer = Renderer()
-        total_lines = 10
-
-        for line in range(total_lines):
-            x0, y0 = matrix_size[1] // 2, matrix_size[0] // 2
-            x1, y1 = int(x0 + (x0 - 20) * math.cos((2 * math.pi * line) / total_lines)), int(
-                y0 + (y0 - 20) * math.sin((2 * math.pi * line) / total_lines))
-            renderer.algorithm_dotted_line(images[0], x0, y0, x1, y1, 20, color)
-            renderer.algorithm_dotted_line_sqrt(images[1], x0, y0, x1, y1, color)
-            renderer.algorithm_x_loop_line(images[2], x0, y0, x1, y1, color)
-            renderer.algorithm_x_loop_line_fixed(images[3], x0, y0, x1, y1, color)
-            renderer.algorithm_dy(images[4], x0, y0, x1, y1, color)
-            renderer.algorithm_bresenham(images[5], x0, y0, x1, y1, color)
-
-        result_images = [Image.fromarray(x, mode="L") for x in images]
-        for image in result_images:
-            image.show()
-
-    @staticmethod
-    def task3():
-        model = ObjModel(path_to_obj_file)
-        vertices = model.parse_vertices()
-        print(vertices)
-
-
-
-    '''
-    @staticmethod
-    def choose_model(model_num):
-        if model_num == 1:
-            path = 'obj-files/model_1.obj'
-            scale = 3000
-            offset = 500
-            model_to_render = ObjModel(path, scale, offset)
-            return model_to_render
-
-        elif model_num == 2:
-            path = 'obj-files/model_2.obj'
-            scale = 0.4
-            offset = 400
-            model_to_render = ObjModel(path, scale, offset)
-            return model_to_render
-
-        elif model_num == 3:
-            path = 'obj-files/model_3.obj'
-            scale = 700
-            offset = 300
-            model_to_render = ObjModel(path, scale, offset)
-            return model_to_render
-        elif model_num == 4:
-            path = 'obj-files/model_4.obj'
-            scale = 4
-            offset = 400
-            model_to_render = ObjModel(path, scale, offset)
-            return model_to_render
-    '''
-
-
-
-
-    @staticmethod
-    def task4(matrix_size: tuple, color, model_num):
-
-        model = TaskManager.choose_model(model_num, matrix_size)
-        renderer = Renderer()
-        image = np.ndarray(matrix_size)
-
-        for (x, y, z) in model.vertices:
-            new_x = int(model.get_scale() * x + model.get_offset())  # 2 mod
-            new_y = int(model.get_scale() * y + model.get_offset())
-            renderer.update_point(image, pos_x=new_x, pos_y=new_y, color=color)
-
-        pil_image = Image.fromarray(image)
-        pil_image = ImageOps.flip(pil_image)
-        pil_image.show()
-
-    @staticmethod
-    def task5():
-        model = ObjModel(path_to_obj_file)
-        faces = model.parse_faces()
-        print(faces)
-
-    @staticmethod
-    def task6(matrix_size: tuple, color: list[int], model_num: int):
-        model = TaskManager.choose_model(model_num, matrix_size)
-        image = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_model_with_faces(image, model, color)
-        file_image = Image.fromarray(image, "RGB")
-        file_image = ImageOps.flip(file_image)
-        file_image.show()
-
-    @staticmethod
-    def task7():
-        print("Оно работает верь мне, функции есть в RendererClass.py")
-
-
-    @staticmethod
-    def task8(matrix_size: tuple, color, x0, x1, x2, y0, y1, y2):
-        image = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_triangle(image=image, color=color, x0=x0, x1=x1, x2=x2, y0=y0, y1=y1, y2=y2)
-        file_image = Image.fromarray(image, "RGB")
-        #file_image = ImageOps.flip(file_image)
-        file_image.show()
-
-    @staticmethod
-    def task9(matrix_size: tuple, color):
-        # completely into bounds
-        image1 = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_triangle(image=image1, color=color, x0=0, x1=200, x2=800, y0=1, y1=500, y2=900)
-        Renderer.draw_triangle(image=image1, color=colors.SILVER, x0=100, x1=300, x2=900, y0=10, y1=600, y2=1000)
-        file_image1 = Image.fromarray(image1, "RGB")
-        file_image1.show()
-
-        # partly out of bounds
-        image2 = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_triangle(image=image2, color=color, x0=-100, x1=200, x2=800, y0=1, y1=5000, y2=900)
-        file_image2 = Image.fromarray(image2, "RGB")
-        file_image2.show()
-
-    @staticmethod
-    def task10(matrix_size: tuple, model_num: int):
-        model = TaskManager.choose_model(model_num, matrix_size)
-        image = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_model_with_random_color_polygons(image, model)
-        file_image = Image.fromarray(image, "RGB")
-        file_image = ImageOps.flip(file_image)
-        file_image.show()
-
-    @staticmethod
-    def task11():
-        print("Оно работает верь мне, функция calculate_normal_to_triangle()")
-
-    @staticmethod
-    def task12(matrix_size: tuple, model_num: int):
-        model = TaskManager.choose_model(model_num, matrix_size)
-        image = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_model_with_random_color_and_cos(image, model)
-        file_image = Image.fromarray(image, "RGB")
-        file_image = ImageOps.flip(file_image)
-        file_image.show()
-
-    @staticmethod
-    def task13(matrix_size: tuple, color: list[int], model_num: int):
-        model = TaskManager.choose_model(model_num, matrix_size)
-        image = np.zeros(matrix_size + (3,), dtype=np.uint8)
-        Renderer.draw_model_with_light(image, color, model)
-        file_image = Image.fromarray(image, "RGB")
-        file_image = ImageOps.flip(file_image)
-        file_image.show()
 
     @staticmethod
     def task14(matrix_size: tuple, color: list[int], model_num: int):
@@ -238,7 +52,7 @@ class TaskManager:
 
     @staticmethod
     def task16(matrix_size: tuple, color: list[int], model_num: int):
-        model = TaskManager.choose_model_new(model_num, matrix_size)
+        model = TaskManager.choose_model(model_num, matrix_size)
         image = np.zeros(matrix_size + (3,), dtype=np.uint8)
 
         z_buffer = np.full(matrix_size, 100000, dtype=np.uint32)
